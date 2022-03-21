c22c963a1caf0638caa176714b34839662f9fa49
Robin Jahn <robin.jahn@st.oth-regensburg.de>
2022-03-21 21:23:35 +0100

    Upload Transitions.java

---
+++ Code/Transitions.java
@@ -0,0 +1,20 @@
+public class Transitions {
+    public static char saveInChar(int x, int y, int r){
+        char mem = (char) (1000 * (short)x + 10 * (short)y + (short)r); //TODO: handle error
+        return mem;
+    }
+
+    public static int getX(char mem){
+         return mem / 1000;
+    }
+
+    public static int getY(char mem) {
+        return (mem%1000)/10;
+    }
+
+    public static int getR(char mem) {
+        return mem%10;
+    }
+
+
+}
