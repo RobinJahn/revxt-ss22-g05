e5bc925440317fdab4318caa24531bf10eeec1a1
Simon Melcher <simon1.melcher@st.oth-regensburg.de>
2022-03-22 11:31:50 +0100

    Added Filedialog

---
+++ Code/src/Dialogfenster.java
@@ -0,0 +1,36 @@
+package src;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+
+public class Dialogfenster {
+
+   
+
+    public String oeffnen() {
+        final JFileChooser chooser = new JFileChooser("Verzeichnis wählen");
+        chooser.setDialogType(JFileChooser.OPEN_DIALOG);
+        chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+        chooser.setFileHidingEnabled(false);
+        final File file = new File("/home");
+
+        chooser.setCurrentDirectory(file);
+
+        chooser.setVisible(true);
+        
+        chooser.showOpenDialog(null);
+
+        if(chooser.getSelectedFile() != null)
+        {
+        	 File inputVerzFile = chooser.getSelectedFile();
+             String inputVerzStr = inputVerzFile.getPath();
+             System.out.println(inputVerzStr);
+             return inputVerzStr;
+        }
+        else
+        {
+        	System.out.println("Keine Datei gewählt");
+        	return null;
+        }
+    }
+} 
