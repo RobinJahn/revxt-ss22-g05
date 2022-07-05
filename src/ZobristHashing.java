134df677d3e0e1ed25409fe18e7fc61cef3bbab0
Simon Melcher <simon1.melcher@st.oth-regensburg.de>
2022-07-05 11:04:54 +0200

    Added CodeDoku to ZobristHashing.java

--- src/ZobristHashing.java
+++ src/ZobristHashing.java
@@ -4,28 +4,29 @@ import java.util.Random;
 
 public class ZobristHashing {
 
-    final private int myStone = 1;
-    final private int notMyStone = 2;
-
     final private int size;
 
     final private int width;
-    final private int height;
 
-    final private int me;
+    final private int playerNumber;
 
-    long[][] zobristTable;
+    final private long[][] zobristTable;
 
     final private long notMyTurn;
 
-    public ZobristHashing(int height, int width, int me)
+    /**
+     *
+     * @param height The height of the Map for which to create a Zobrist Table for.
+     * @param width The width of the Map for which to create a Zobrist Table for.
+     * @param playerNumber The Player Number of the Client for this map.
+     */
+    public ZobristHashing(int height, int width, int playerNumber)
     {
         Random RandomGen = new Random();
 
         size = height*width;
         this.width = width;
-        this.height = height;
-
+        this.playerNumber = playerNumber;
 
         zobristTable = new long[size][2];
         for(int i= 0; i< size;i++)
@@ -36,26 +37,28 @@ public class ZobristHashing {
             }
         }
         notMyTurn = RandomGen.nextLong();
-
-        this.me = me;
     }
 
+    /**
+     *
+     * @param map Is the Map for which a hash is to generate.
+     * @return Returns the Hashed Value of a given Map.
+     */
     public long hash(Map map)
     {
         long hashValue = 0;
 
-        if(map.getCurrentlyPlayingI() == me)
+        if(map.getCurrentlyPlayingI() == playerNumber)
         {
             hashValue ^= notMyTurn;
         }
-
 
         for(int i = 1;i<map.getAnzPlayers();i++)
         {
            for (Position p: map.getStonesOfPlayer(i))
             {
                 int pos = p.y*width+p.x;
-                if(i == me)
+                if(i == playerNumber)
                 {
                     hashValue ^= zobristTable[pos][0];
                 }
@@ -68,6 +71,9 @@ public class ZobristHashing {
         return hashValue;
     }
 
+    /**
+     * Prints the Values of the Zobrist Table in two Rows and the Value for being not my turn.
+     */
     public void printZobristTable()
     {
         System.out.println("Zobrist Table: ");
