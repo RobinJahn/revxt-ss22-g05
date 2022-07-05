14b7150386fa2c727dc129226377c273ee7babd6
Simon Melcher <simon1.melcher@st.oth-regensburg.de>
2022-07-05 11:25:08 +0200

    Refactored and Added CodeDoku to TranspositionTable.java

--- src/TranspositionTable.java
+++ src/TranspositionTable.java
@@ -10,18 +10,30 @@ public class TranspositionTable {
     private long TranspositionMiss = 0;
     private long Replacements = 0;
 
+    /**
+     * Creates a Transposition Table of the given Size.
+     * @param size The Size of the TranspositionTable
+     */
     public TranspositionTable(int size)
     {
         this.size = size;
         TranspositionArray = new Transposition[size];
     }
 
-
+    /**
+     * Empties the Transposition Table
+     */
     public void empty()
     {
         TranspositionArray = new Transposition[size];
     }
 
+    /**
+     * Tries to find the given hash in the Transposition Table and returns the Value of the Map in case of a Hit or in Case of a Miss a Default Value.
+     * @param hash The Hash Value to look up
+     * @param depth The Current Depth
+     * @return Returns the Value of the given Map or the Min Value of a Double + 1
+     */
     public double lookUp(long hash, int depth)
     {
         int index = (int)hash%size;
@@ -42,6 +54,12 @@ public class TranspositionTable {
         return Double.MIN_VALUE+1;
     }
 
+    /**
+     * Adds the Value of a Map represented by the hash Value to the Transposition Table for a given depth.
+     * @param hash The Hash Value of the Map
+     * @param depth The Current Depth
+     * @param value The Value of the Map
+     */
     public void add(long hash, int depth, double value)
     {
         int index = (int)hash%size;
@@ -50,29 +68,48 @@ public class TranspositionTable {
             index *=-1;
         }
 
-        Transposition transposition = new Transposition(hash,depth,value);
+        Transposition transposition = new Transposition(hash, depth, value);
+
+        if(TranspositionArray[index] != null)
+        {
+            Replacements++;
+        }
 
         TranspositionArray[index] = transposition;
     }
 
+    /**
+     * Returns the Number of Transposition Hits.
+     * @return The Number of Transposition Hits
+     */
     public long getTranspositionHits()
     {
         return TranspositionHits;
     }
+
+    /**
+     * Returns the Number of Transposition Misses.
+     * @return The Number of Transposition Misses
+     */
     public long getTranspositionMiss() { return  TranspositionMiss;}
+
+    /**
+     * Returns the Number of Entries which have been replaced.
+     * @return The Number of Replacements
+     */
     public long getReplacements() { return Replacements;}
-}
 
- class Transposition
-{
-    long hash;
-    double value;
-    int depth;
-
-    public Transposition(long hash, int depth, double value)
+    private static class Transposition
     {
-        this.hash = hash;
-        this.depth = depth;
-        this.value = value;
+        long hash;
+        double value;
+        int depth;
+
+        public Transposition(long hash, int depth, double value)
+        {
+            this.hash = hash;
+            this.depth = depth;
+            this.value = value;
+        }
     }
 }
