759b941a9014167a018f22d99d07a644c5aa3232
Simon-Melcher <82505950+Simon-Melcher@users.noreply.github.com>
2022-07-17 17:44:49 +0200

    Refactored and Added Code Documentation to KillerArray and KillerLine has been made a private class in KillerArray

--- src/KillerArray.java
+++ src/KillerArray.java
@@ -2,25 +2,23 @@ package src;
 
 public class KillerArray {
 
-    private KillerLine[] killerLineArray;
-
+    private final KillerLine[] killerLineArray;
     private int length = 0;
+    private final int maxLength = 10;
 
-    //@Todo Evaluate cuts by height
-    //@Todo Parameter testing
-    private int maxLength = 10;
-
-    public KillerArray(int size)
-    {
-        this.killerLineArray = new KillerLine[size];
-        this.maxLength = size;
-    }
-
+    /**
+     * This Constructor creates a new KillerArray of a given Size of 10.
+     */
     public KillerArray()
     {
-        this.killerLineArray = new KillerLine[10];
+        this.killerLineArray = new KillerLine[maxLength];
     }
 
+    /**
+     * This Function returns the Position and Additional Information of a Move in the KillerArray
+     * @param index The Index of the Position to take
+     * @return Returns the Position and Information of an entry in the KillerArray.
+     */
     public int[] getPositionAndInfo(int index)
     {
         if(index<= length) {
@@ -29,16 +27,25 @@ public class KillerArray {
         return null;
     }
 
+    /**
+     * This Function returns the Length of the KillerArray.
+     * @return Returns the Length of the KillerArray.
+     */
     public int getLength()
     {
         return length;
     }
 
+    /**
+     * This Function Adds a new Move to the KillerArray, if there is Space available or the move cuts more moves than the currently worst move saved.
+     * @param PositionAndInfo The New Move to Check
+     * @param NumberOfCutLeaves The Number of Leaves the Move cut.
+     */
     public void add(PositionAndInfo PositionAndInfo, int NumberOfCutLeaves)
     {
         //If Space is available
         if(length < maxLength)
-        {   killerLineArray[length] = new KillerLine(PositionAndInfo,NumberOfCutLeaves);
+        {   killerLineArray[length] = new KillerLine(PositionAndInfo, NumberOfCutLeaves);
             length++;
         }
         //If No Space left
@@ -67,6 +74,32 @@ public class KillerArray {
                     killerLineArray[j] = temp;
                 }
             }
+        }
+    }
+    private static class KillerLine {
+        private PositionAndInfo Pos;
+        private int NumberOfCutLeaves;
+
+        private KillerLine(PositionAndInfo pos, int NumberOfCutLeaves)
+        {
+            this.Pos = pos.clone();
+            this.NumberOfCutLeaves = NumberOfCutLeaves;
+        }
+
+        private PositionAndInfo getPositionAndInfo() {
+            return Pos;
+        }
+
+        private int getNumberOfCutLeaves() {
+            return NumberOfCutLeaves;
+        }
+
+        private void setPositionAndInfo(PositionAndInfo pos) {
+            Pos = pos;
+        }
+
+        private void setNumberOfCutLeaves(int numberOfCutLeaves) {
+            NumberOfCutLeaves = numberOfCutLeaves;
         }
     }
 }
