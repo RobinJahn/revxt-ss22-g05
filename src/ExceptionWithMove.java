ceeaeebe70f3279b062ccccfc7f701ca6e8cdbbb
RobinJahn <robin@kamerapilot.com>
2022-07-21 10:33:35 +0200

    Commented ExceptionWithMove and MctsNode (JavaDoc)

--- src/ExceptionWithMove.java
+++ src/ExceptionWithMove.java
@@ -5,6 +5,12 @@ import java.util.concurrent.TimeoutException;
 public class ExceptionWithMove extends TimeoutException {
     public int[] PosAndInfo;
 
+    /**
+     * This Constructor creates an exception that contains a Position.
+     * For example this can be used when the time is up when searching for all possible moves.
+     * The Position then could represent a possible move the player could make.
+     * @param posAndInfo int array that specifies the move. (x, y, info)
+     */
     public ExceptionWithMove(int[] posAndInfo){
         this.PosAndInfo = posAndInfo;
     }
--- src/MctsNode.java
+++ src/MctsNode.java
@@ -4,16 +4,29 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MctsNode {
-    private Map map;
-    private ArrayList<MctsNode> childs = new ArrayList<>();
-    private MctsNode parent;
-    private int[] actionLeadingToThis;
+
+    //final
+    private final Map map;
+    private final ArrayList<MctsNode> childs = new ArrayList<>();
+    private final MctsNode parent;
+    private final int[] actionLeadingToThis;
+    private final ArrayList<int[]> possibleMoves;
+    private final boolean phaseOne;
+    private final boolean terminal;
+
+    //not final
     private int countOfVisits;
     private double reward;
-    private ArrayList<int[]> possibleMoves = null;
-    private boolean phaseOne;
-    private boolean terminal;
 
+    /**
+     * This Constructor creates an MCTS-Node. This is used for the Monte-Carlo-Tree-Search algorithm.
+     *
+     * @param map The map the game state has.
+     * @param parent The parent MCTS-Node. If it has no parent, it can be set to null.
+     * @param posAndInfoLeadingToThis This int array stores a move that lead to this game state. (x, y, info)
+     * @param possibleMoves This represents a list of Moves that could follow this game state. The Move is stored in an int array of the form (x, y, info)
+     * @param phaseOne This boolean contains the current phase of the game state.
+     */
     public MctsNode(Map map, MctsNode parent, int[] posAndInfoLeadingToThis, ArrayList<int[]> possibleMoves, boolean phaseOne){
         this.map = map;
         this.parent = parent;
@@ -26,22 +39,35 @@ public class MctsNode {
         else terminal = false;
     }
 
+    /**
+     * This method returns a boolean that represents if the game state is terminal.
+     * @return returns true if the game state is terminal and false otherwise.
+     */
     public boolean isTerminal(){
         return terminal;
     }
 
+    /**
+     * This method returns a boolean that represents if there are moves left, that weren't already checked.
+     * @return returns true if the node is fully extended.
+     */
     public boolean isFullyExpanded(){
         return possibleMoves.isEmpty();
     }
 
+    /**
+     * This method addes a child node to the node it is called on.
+     * @param newChild The child that should be added.
+     */
     public void addChild(MctsNode newChild){
         childs.add(newChild);
     }
 
-    public ArrayList<int[]> getUntriedActions(){
-        return possibleMoves;
-    }
-
+    /**
+     * This method returns a random move that wasn't checked already. Also it marks the move it returns as used.
+     * Before calling this method it needs to be checked if the node is fully expanded.
+     * @return Move that wasn't checked already.
+     */
     public int[] getUntriedActionAndRemoveIt(){
         int index = (int)Math.round(Math.random()*(possibleMoves.size()-1));
         int[] result = possibleMoves.get( index );
@@ -49,39 +75,72 @@ public class MctsNode {
         return result;
     }
 
-
+    /**
+     * @return Returns a boolean that represents the reward that this node has.
+     */
     public double getTotalReward(){
         return reward;
     }
 
+    /**
+     * This method returns the ammount of visits this node had.
+     * @return number of visits.
+     */
     public int getCountOfVisits(){
         return countOfVisits;
     }
 
+    /**
+     * This Method returns a list of all childs the node has.
+     * @return List of childs.
+     */
     public ArrayList<MctsNode> getChilds() {
         return childs;
     }
 
+    /**
+     * This Method returns the Map of the game state the node represents.
+     * @return the Map object
+     */
     public Map getMap(){
         return map;
     }
 
+    /**
+     * This method increases the visits the node had.
+     */
     public void increaseVisits(){
         countOfVisits++;
     }
 
+    /**
+     * This method addes delta to the reward of the node
+     * @param delta the value that gets added to the reward.
+     */
     public void updateReward(double delta){
         reward+=delta;
     }
 
+    /**
+     * This method returns the parent of the node
+     * @return the parent MCTS-Node
+     */
     public MctsNode getParent(){
         return parent;
     }
 
+    /**
+     * This method returns the move that lead to this game state.
+     * @return int array that represents the move (x, y, info)
+     */
     public int[] getActionLeadingToThis(){
         return actionLeadingToThis;
     }
 
+    /**
+     * This method returns a boolean that represents if the game state of this node is in the first, or the second phase.
+     * @return Returns true if the game state is in the first phase and false otherwise.
+     */
     public boolean isPhaseOne(){
         return phaseOne;
     }
